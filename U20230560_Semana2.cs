74409182fc68a9b05fe7447ac7299b56d02ce96c
ezequ <ezequielhernandes907@gmail.com>
2023-08-02 13:23:37 -0600

    Guía#2

---
+++ U20230560_Semana2.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //DECLARACIÓN DE VARIABLES
+
+            //ITEMS 1
+
+            int edad= 15;
+            
+            string objeto="laptop";
+
+            //ITEMS 2
+
+            float salario=502.25;
+
+            decimal precio= 20.3658;
+
+            //ITEMS 3
+
+            bool activo= true;
+
+
+            //AUTOEVALUACIÓN 10
+
+
+            
+
+
+        }
+        
+
+
+
+
+    }
+}
